a7d1087c63d63df37d67e87d498590d47b8d45ec
yshak <Vyshakn29@gmail.com>
2025-04-14 00:18:16 +0530

    added the product

--- app/sections/Products.tsx
+++ app/sections/Products.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import { Pattern1 } from '@/public'
+import { Pattern1,Pattern2 } from '../../public/index'
+import Marquee from 'react-fast-marquee'
 
 function Products() {
     return (
         <section className='w-full gap-y-12 flex-col flex justify-between items-center'>
-            <span className='text-white font-semibold  text-3xl'>"Crafted by Tradition,  Carried Through Time"</span>
+            <span className='text-white font-semibold  text-3xl'>Crafted by Tradition,  Carried Through Time</span>
             <div className='w-full gap-y-8 flex justify-center items-center flex-col'>
                 <div className='w-full h-96 overflow-hidden flex items-center'>
                     <Image alt='pattern1' src={Pattern1} className='w-auto ' />
@@ -16,7 +17,11 @@ function Products() {
                 </div>
                 <button className='px-20 py-4 bg-KalaYellow-500 font-semibold text-neutral-800'>EXPLORE</button>
             </div>
-
+            <Marquee>
+            <div className='w-full overflow-hidden h-16'>
+                <Image alt='pattern2' src={Pattern2} />
+            </div>
+            </Marquee>
         </section>
     )
 }
--- public/index.ts
+++ public/index.ts
@@ -1,6 +1,7 @@
 import LogoLg from './Logo/LogoLg.svg'
 import Upcoming from "./images/Upcomingevents.png"
 import Pattern1 from "./Products/Pattern1.svg"
+import Pattern2 from "./Products/Pattern2.svg"
 
 
-export {LogoLg,Upcoming,Pattern1}
+export {LogoLg,Upcoming,Pattern1,Pattern2}
