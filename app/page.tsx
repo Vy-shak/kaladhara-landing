fe37499be505ae440c873edcaca1b16faab531c1
yshak <Vyshakn29@gmail.com>
2025-05-08 15:05:22 +0530

    curousal

--- app/page.tsx
+++ app/page.tsx
@@ -33,10 +33,10 @@ export default function Home() {
         <section className="w-full px-4 sm:px-8 lg:px-12 xl:px-16 2xl:px-20 flex flex-col  justify-center items-center">
         <About/>
        </section>
-        {/* <section className="w-full pt-20 ">
+        <section className="w-full pt-20 ">
         <Carousel slides={slideData}/>
         </section>
-        <section className="w-full pt-20 ">
+        {/* <section className="w-full pt-20 ">
         <Performance/>
         </section>
         <section className="w-full pt-20 ">
