f735048440ab3b0c8935310abf79ab818db1bc18
yshak <Vyshakn29@gmail.com>
2025-05-10 23:47:02 +0530

    contact card done

--- app/components/Contact/Form.tsx
+++ app/components/Contact/Form.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Form() {
     return (
-        <div className='w-full flex flex-col items-start justify-center'>
+        <div className='w-full gap-y-4 flex flex-col items-start justify-center'>
             <div className='w-full gap-y-1 flex flex-col justify-center items-start'>
                 <span className='text-sm'>Name</span>
                 <input className='w-full h-12 rounded bg-neutral-700' type="text" />
--- app/components/macro/Heading.tsx
+++ app/components/macro/Heading.tsx
@@ -8,7 +8,7 @@ function Heading({ title }: heading) {
   return (
     <div className='flex gap-y-2 justify-center items-center flex-col'>
       <div className='w-fit flex gap-y-2 justify-center items-center flex-col'>
-        <span className='text-white font-semibold  text-3xl'>{title}</span>
+        <span className='text-white font-semibold text-center  text-3xl'>{title}</span>
         <div className='flex justify-center items-center w-full'>
           <div className='w-1/3 bg-KalaRed-500 h-1.5'>
 
